1319c591e05e736e3393fe6ca299450b47246a33
Laszlo Balogh <laszlo@thebaloghs.com>
2016-10-13 04:18:04 -0600

    remove dev_input_array

--- lib/datenight.rb
+++ lib/datenight.rb
@@ -1,9 +1,6 @@
 require_relative 'binarysearchtree'
 require 'pry'
 
-dev_input_list = [[18, "Terminator"], [25, "Back To The Future"], [65, "Star Wars"], [34, "Cars"], [2, "Spy Game"]]
-
-
 tree = BinarySearchTree.new
 #Copied from DateNight Project markdown
 insert61 = tree.insert(61, "Bill & Ted's Excellent Adventure")
